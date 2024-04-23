787415fdca592183fe2a36b06c7f15c85f68b10b
Francisco de Borja Cabeza Rozas <borjacabezarozas@gmail.com>
2024-04-23 09:31:08 +0200

    Commit demo

---
+++ Contoso.Andromeda.ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.SqlClient;
+
+
+
+namespace Contoso.Andromeda.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var alumno = new Alumno();   
+
+        }
+
+    }
+
+    public class Alumno
+    { 
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+    }
+
+
+}
---
+++ Contoso.Andromeda.WebClientes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Contoso.Andromeda.WebClientes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
